fb67ccdda0ca719fba55e4c061ad7a12e4ffa1fe
vagrant <vagrant@homestead>
2019-05-16 11:26:44 +0000

    首页微博列表

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -7,6 +7,9 @@
         <section class="status_form">
           @include('shared._status_form')
         </section>
+        <h4>微博列表</h4>
+        <hr>
+        @include('shared._feed')
       </div>
       <aside class="col-md-4">
         <section class="user_info">
@@ -16,9 +19,9 @@
     </div>
   @else
     <div class="jumbotron">
-      <h1>Hello</h1>
+      <h1>Hello Laravel</h1>
       <p class="lead">
-        你现在所看到的是Blog主页。
+        你现在所看到的是示例项目主页。
       </p>
       <p>
         一切，将从这里开始。
